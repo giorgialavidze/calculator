d86b8764c4a062b76568ec57607d7ad3c0bc5523
giorgialavidze <giorgimaisuradze564@gmail.com>
2022-12-18 02:28:45 +0400

    refactored index.js

--- index.js
+++ index.js
@@ -5,22 +5,49 @@ class Calculator {
         this.secondNumberElement = config.secondNumberElement;
         this.summaryElement = config.summaryElement;
         
+        this.stage = "getFirstNumber";
+        this.operator = "";
         this.firstNumber = "";
         this.operands = ["+","-","*","/","=","AC","DEL"];
     }
+    
+    PressKey(key){
+        if (this.operands.includes(key)) return this.pressOperator(key);
+        this.pressNumber(key);
+        
+    }
 
-    saveFirstNumber(number){
-        if (this.operands.includes(number)) return;
+    pressNumber(number){
+        if (this.stage === "getFirstNumber") {
+            this.compileFirstNumber(number);
+        }
+        console.log(number);
+    }
+
+    pressOperator(operator){
+        if (this.stage === "getFirstNumber") {
+            this.stage = "getSecondNumber";
+            this.operator = operator;
+            this.drawOperand(this.operatorElement,this.operator);
+        }
+        console.log(operator);
+
+    }
+
+    //save input first number by user and show it on the screen
+    compileFirstNumber(number){
+        // input symbol is operator or not
+        
+
         this.firstNumber += number;
         this.drawNumber(this.firstNumberElement,this.firstNumber) ; 
     }
     
     drawNumber(element,number){
         element.innerHTML = number;
-
     }
-    drawOperand(){
-
+    drawOperand(element,operator){
+        element.innerHTML = operator;
     }
 }
 
@@ -36,8 +63,7 @@ const keyboard = document.querySelectorAll(".key");
 keyboard.forEach(key => {
     key.addEventListener("click", () => {
         const value = key.innerHTML;
-        calculator.saveFirstNumber(value);
-        
+        calculator.PressKey(value);     
     });
 });
 
